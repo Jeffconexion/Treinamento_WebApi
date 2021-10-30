ecf98506ebac967f9a9eedaf7a6ea5b5b1770d85
Jefferson S <jefferson.santos@radixeng.com.br>
2021-10-30 15:38:58 -0300

    Criando o crud de fornecedor.

---
+++ AppWebApiCore/AppWebApiCore/Data/ApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AppWebApiCore.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppWebApiCore.Data
+{
+    public class ApiDbContext : DbContext
+    {
+
+        public ApiDbContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+    }
+}
